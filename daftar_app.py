b52d2f2b7283a8b2df871fc22119fe4b181203a9
safrasia <safrasia@qti.qualcomm.com>
2022-09-10 22:05:09 +0200

    better error handling in the wrapper

--- daftar_app.py
+++ daftar_app.py
@@ -4,10 +4,11 @@ Saeed Afrasiabi
 """
 
 from tkinter import messagebox
+import traceback as tb
 
 try:
     from daftar_gui import DaftarGui
     myApp=DaftarGui()
     myApp.run()
 except Exception as e:
-    messagebox.showerror("Wrapper-caught error", "The following error was caught by the app wrapper:\n"+str(e))
+    messagebox.showerror("Wrapper-caught error", "The following error was caught by the app wrapper:\n"+str(e) + "\n" + tb.format_exc())
